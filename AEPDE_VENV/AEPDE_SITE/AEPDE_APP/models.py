399225a2604d7c03c22587770cbb52607be129bf
xowito <xowito01@agmail.com>
2023-05-10 00:53:08 -0400

    Termino AEPDE-3 y avance AEPDE-4 + registration

--- AEPDE_VENV/AEPDE_SITE/AEPDE_APP/models.py
+++ AEPDE_VENV/AEPDE_SITE/AEPDE_APP/models.py
@@ -4,8 +4,9 @@ from django.db import models
 class Sucursal(models.Model):
     cod_sucursal = models.CharField(primary_key=True,max_length=25)
     direccion = models.CharField(max_length=500)
+    nombre = models.CharField(max_length=500)
     def __str__(self):
-	    return self.cod_sucursal
+	    return self.nombre + '-'+ self.direccion
 class Producto(models.Model):
     tipo_producto = [
         ('1','Frutas'),
@@ -25,3 +26,8 @@ class Producto(models.Model):
 	    return self.codigo
 class Meta:
     ordering = ["precio"]
+    
+class Productor(models.Model):
+    cod_productor = models.CharField(primary_key=True,max_length=9)
+    nombre_empresa = models.CharField(max_length=500)
+    
